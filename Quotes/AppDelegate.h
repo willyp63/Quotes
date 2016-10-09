3ede623923ace59489638f9c73b278892a0b1c1f
Wil Pirino <wpirino1@gmail.com>
2016-10-08 19:30:06 -0700

    Initial Commit

---
+++ Quotes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quotes
+//
+//  Created by Wil Pirino on 10/8/16.
+//  Copyright © 2016 Wil Pirino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
